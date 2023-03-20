4973f34024d398085cbc791a9453dd24ab55bd2f
uniaue <115631356+uniaue@users.noreply.github.com>
2023-03-20 09:50:01 +0100

    ready for submission

---
+++ index.js
@@ -0,0 +1,9 @@
+const { sum, calculator, subtract } = require ("./maths");
+// sum()
+
+calculator.multiply();
+
+
+console.log(sum(4, 5));
+console.log(calculator.multiply(8,5))
+console.log(subtract(20,12));
---
+++ maths.js
@@ -0,0 +1,10 @@
+const sum = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+
+const calculator = {
+     multiply: (q,t) =>{
+        return  q * t
+    }
+}
+
+ module.exports = {sum, calculator,subtract};
